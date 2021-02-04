c3b145788ca4aba1dcc90e442fed883ba0f149f4
jnicholsthru <jnichols@sailthru.com>
2021-02-04 11:59:17 -0500

    add = to requests/limits

--- deployment.tf
+++ deployment.tf
@@ -45,12 +45,12 @@ resource "kubernetes_deployment" "this" {
           termination_message_path = "/dev/termination-log"
 
           resources {
-            requests {
+            requests = {
               memory = var.resources_requests_memory
               cpu    = var.resources_requests_cpu
             }
 
-            limits {
+            limits = {
               memory = var.resources_limits_memory
               cpu    = var.resources_limits_cpu
             }
